4d8e928065506d6ebe1838f042f0e46c9a2f8f73
Nikita Podzhidaev <ppodzhidaevv@gmail.com>
2026-05-08 02:28:24 +0700

    Entity layer almost done. Added SellerEntity && TransactionEntity

---
+++ src/main/java/shift/lab/crm/persistence/SellerEntity.java
@@ -0,0 +1,33 @@
+package shift.lab.crm.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table(name = "sellers")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SellerEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "contact_info", nullable = false)
+    private String contactInfo;
+
+    @Column(name = "registration_date", nullable = false)
+    private LocalDateTime registrationDate;
+
+}
---
+++ src/main/java/shift/lab/crm/persistence/TransactionEntity.java
@@ -0,0 +1,38 @@
+package shift.lab.crm.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "transactions")
+@Entity
+public class TransactionEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "seller", nullable = false)
+    private SellerEntity seller;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "payment_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private String paymentType;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDateTime transactionDate;
+
+}
